1b7b83473c0c2c7e1eb3bad8863194c5ef562bd3
Music-Mad <chrisklepper1776@gmail.com>
2025-10-07 11:39:45 -0400

    moved main board route to sever.js to open board.js for api endpoints

--- routes/board.js
+++ routes/board.js
@@ -6,13 +6,6 @@ const router = express.Router();
 import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-router.get('/board', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'board.html'), (err) => {
-        if (err) {
-            console.error(err);
-        }
-    });
-});
 
 
 export default router;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 //  IMPORTS 
 import express from 'express';
 import codesRouter from './routes/codes.js';
-import boardRouter from './routes/board.js';
 import notesRouter from './routes/notes.js';
 
 const app = express();
@@ -18,10 +17,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Mount route handlers
 app.use('/api', codesRouter);
 app.use('/api', notesRouter);
-app.use('/', boardRouter);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/board', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'board.html'), (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
 });
 
 //start server
