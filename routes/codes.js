1b3685b573979115029c9605a1a1ae18f00e3bd3
Music-Mad <chrisklepper1776@gmail.com>
2025-10-10 19:47:11 -0400

    Fixed codes route by using updated boardController function names

--- routes/codes.js
+++ routes/codes.js
@@ -5,10 +5,10 @@ const router = express.Router();
 router.post('/board-code', (req, res) => {
     const code = req.body.boardCode; 
     //If room does not exist, create one
-    let boardData = boardController.getBoard(code);
+    let boardData = boardController.get(code);
     if (typeof boardData == 'undefined') {
-        boardController.createBoard(code);
-        boardData = boardController.getBoard(code);
+        boardController.create(code);
+        boardData = boardController.get(code);
     }
 
     res.redirect(`/board?boardCode=${code}`);
