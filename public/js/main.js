e077c477e809111e388536ebca4792bb2d58d550
Music-Mad <chrisklepper1776@gmail.com>
2025-10-24 17:58:28 -0400

    Added save state monitoring that sends data back to database when user is done making changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,47 @@
 
 document.addEventListener('DOMContentLoaded', async function () {
     console.log('Page loaded!');
-
+    //get board code
     const urlParams = new URLSearchParams(window.location.search);
     const boardCode = urlParams.get('boardCode');
     loadObjects(boardCode);
 
+    //Updates board if no changes are made within 3 seconds
+    let hasChanges = false;
+    let saveTimeout;
+    const SAVE_DELAY = 3000;
+    const BACKUP_TIMER = 15000;
+
+    function onStateChange() {
+        hasChanges = true;
+        clearTimeout(saveTimeout);
+        saveTimeout = setTimeout(() => {
+            saveBoard(boardCode);
+            hasChanges = false;
+        }, SAVE_DELAY);
+    }
+    StickyManager.onChange(onStateChange);
+
+    //periodically save document just in case
+    setInterval(() => {
+        if (hasChanges) {
+            console.log('backupsave')
+            saveBoard(boardCode);
+            hasChanges = false;
+        }
+    }, BACKUP_TIMER);
+
+
     const stickyPad = document.getElementById('sticky-pad');
     stickyPad.addEventListener('mousedown', async (e) => {
         const note = await NotesAPI.create(null, '#fff475', e.clientX - 180, e.clientY, 1, boardCode);
-        const noteDom = StickyManager.create(null, '#fff475', e.clientX - 160, e.clientY, note.id);
+        const noteDom = StickyManager.create(null, '#fff475', e.clientX - 160, e.clientY, null, note.id);
+
         StickyManager.startDragging(note.id, e.clientX, e.clientY);
     });
 
-    //update loop
-    const intervalId = setInterval(async () => {
-        console.log('Updating');
-        notes = StickyManager.getAll();
-        if (notes == null) {
-            return;
-        }
-    
-        for (var i = 0; i < notes.length; i++) {
-            NotesAPI.update(notes[i].id, {x_position: parseInt(notes[i].style.left), y_position: parseInt(notes[i].style.top)});
-        }
-        
-    }, 1000);
+
+
+
+
 });
